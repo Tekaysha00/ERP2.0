ca494eea9b91587a4a390abe8ed94b96bc10c00b
Tekaysha00 <tausifkamal001@gmail.com>
2026-05-12 12:09:52 +0530

    Add files via upload

--- app/models/notice_model.py
+++ app/models/notice_model.py
@@ -7,10 +7,23 @@ class Notice(db.Model):
     __tablename__ = "notices"
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable=False)
-    message = db.Column(db.Text, nullable=False)
 
-    target = db.Column(db.String(20), default="all")  
-    # options: 'all', 'student', 'teacher'
+    # Subject
+    title = db.Column(db.String(200), nullable=False)
+
+    # Issue / Description
+    message = db.Column(db.Text, nullable=True)
+
+    # student / teacher
+    target = db.Column(db.String(20), nullable=False)
+
+    # Student notice ke liye
+    classname = db.Column(db.String(50), nullable=True)
+
+    # Teacher notice ke liye
+    teacher_id = db.Column(db.Integer, db.ForeignKey("teachers.id"), nullable=True)
+
+    # attachment
+    attachment = db.Column(db.String(300), nullable=True)
 
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
