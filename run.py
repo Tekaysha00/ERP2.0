deb0102ffde801fea2f4776161e34041dbc31ed1
Tekaysha00 <tausifkamal001@gmail.com>
2026-05-19 16:14:04 +0530

    Add files via upload

--- run.py
+++ run.py
@@ -1,10 +1,37 @@
 from app import create_app
 from flask import Flask
 
+import time
+from flask import g, request
 
 
 
 app = Flask(__name__)
+
+@app.before_request
+def before_request():
+    g.start_time = time.time()
+
+
+@app.after_request
+def after_request(response):
+    if hasattr(g, 'start_time'):
+        duration = time.time() - g.start_time
+
+        print(
+            f"""
+================ API PERFORMANCE ================
+PATH       : {request.path}
+METHOD     : {request.method}
+TIME       : {duration:.2f} sec
+STATUS     : {response.status_code}
+=================================================
+"""
+        )
+
+    return response
+
+
 
 app = create_app()
 
