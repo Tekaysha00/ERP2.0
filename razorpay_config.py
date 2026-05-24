f2078eae2caa178c15f4a63b4ccb985436a29f31
Tekaysha00 <tausifkamal001@gmail.com>
2026-05-24 17:02:05 +0530

    Add files via upload

--- razorpay_config.py
+++ razorpay_config.py
@@ -1,6 +1,2 @@
-import razorpay
-
 RAZORPAY_KEY_ID = "rzp_test_20tkfyOZteuJyu"
-RAZORPAY_KEY_SECRET = "bMrRXLvsfNu2ij51fcn3UZPu"
-
-razorpay_client = razorpay.Client(auth=(RAZORPAY_KEY_ID, RAZORPAY_KEY_SECRET))
+RAZORPAY_SECRET = "bMrRXLvsfNu2ij51fcn3UZPu"
